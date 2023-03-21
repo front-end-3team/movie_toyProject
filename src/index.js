4afdac27e551bf2144a36bc7facb5bb74de70e90
taeginote <ab315601@naver.com>
2023-03-21 16:38:50 +0900

    Feat: 리액트쿼리 설정중

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import App from './App';
+
+const queryClient = new QueryClient(); //react-query를 사용하기 위한 인터스턴 생성
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
+	<QueryClientProvider client={queryClient}>
+		<ReactQueryDevtools initialIsOpen={true} />
 		<App />
-	</React.StrictMode>,
+	</QueryClientProvider>,
 );
