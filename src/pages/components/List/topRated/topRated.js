eeb43cfab7cd4302dfc89ee1d49b7df468994547
seonyeong719 <sy904280@gmail.com>
2023-03-23 20:58:36 +0900

    Feat: top-rated 페이지 완료

--- src/pages/components/List/topRated/topRated.js
+++ src/pages/components/List/topRated/topRated.js
@@ -1,2 +1,37 @@
-function TopRated() {}
+import React from 'react';
+import { useInfiniteQuery } from 'react-query';
+import styled from 'styled-components';
+import { getTopRated } from '../../../../apis/api';
+import CommonList from '../commonList';
+
+function TopRated() {
+	const { isLoading, isError, error, data, hasNextPage, fetchNextPage } =
+		useInfiniteQuery(['topRated'], getTopRated, {
+			getNextPageParam: currentPage => {
+				const nextPage = currentPage.page + 1;
+				return nextPage > currentPage.total_pages ? null : nextPage;
+			},
+		});
+
+	return (
+		<>
+			<H1>Top Rated</H1>
+			<CommonList
+				data={data}
+				isLoading={isLoading}
+				isError={isError}
+				fetchNextPage={fetchNextPage}
+				hasNextPage={hasNextPage}
+				error={error}
+			/>
+		</>
+	);
+}
 export default TopRated;
+
+const H1 = styled.h1`
+	background-color: rgb(132, 132, 132);
+	font-size: 60px;
+	font-weight: bold;
+	padding-left: 50px;
+`;
